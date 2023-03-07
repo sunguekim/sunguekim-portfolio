b963540e5699a2ba9a2852e100445877035a6c3b
sunguekim <sunguekim96@gmail.com>
2023-03-07 19:32:03 +0900

    add__asset 추가

--- component/Home.tsx
+++ component/Home.tsx
@@ -37,11 +37,11 @@ const Home = () => {
                         또한, 동료와의 소통과 협업이 개발 프로젝트에서의 성공에 큰 영향을 미친다는 것을 알고 있습니다. 개발 팀의 목표를 달성하기 위해 동료들과 상호 존중과 소통의 적극적인 관심을 가지고 있습니다.
                         저는 새로운 경험과 도전을 통해 성장할 수 있는 기회를 기다리고 있습니다.
                     </p>
-                    <Stack direction="row" spacing={2} sx={{justifyContent:'center',marginTop:4}}>
-                        <Button variant="outlined" startIcon={<GithubIcon />}>
+                    <Stack direction="row" spacing={2} sx={{justifyContent:'flex-start',marginTop:4}}>
+                        <Button variant="contained" startIcon={<GithubIcon />}>
                             Github
                         </Button>
-                        <Button variant="contained" endIcon={<AssessmentIcon />}>
+                        <Button variant="outlined" endIcon={<AssessmentIcon />}>
                             이력서
                         </Button>
                     </Stack>
