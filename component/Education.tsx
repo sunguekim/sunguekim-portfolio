4af53eaf85429f2130bcb268a2e820346881636f
sunguekim <sunguekim96@gmail.com>
2023-03-08 19:37:16 +0900

    fix__학습 컴포넌트 이미지 수정&추가

--- component/Education.tsx
+++ component/Education.tsx
@@ -1,20 +1,35 @@
 import React from 'react'
+import Image from "next/image";
 import { Fade } from 'react-awesome-reveal'
 import Subtitle from './UI/Subtitle'
 import { education } from '../contexts/data/educationData'
 import { IEducation } from '../contexts/props'
 import EduationCard from './UI/EduationCard'
-import '../styles/Education.module.css'
+import eduGreen from '../public/assets/eduGreen.png'
+import styles from '../styles/Education.module.css'
 
 
 const Education: React.FC = () => {
     return (
-        <section id='education'>
+        <div id='education' >
             <Subtitle subtitle='Education' />
-            {education && education.map((data: IEducation) => (
-                <EduationCard key={data.id} {...data} />
-            ))}
-        </section>
+            <div className={`${styles.education__container}`}>
+                <div className={`${styles.education__detail}`}>
+                    {education && education.map((data: IEducation) => (
+                        <EduationCard key={data.id} {...data} />
+                    ))}
+                </div>
+                <div className={`${styles.education__img}`}>
+                    <Image
+                        src={eduGreen}
+                        width="600px"
+                        height="600px"
+                        objectFit="contain"
+                        alt="프로필 이미지"
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
--- component/UI/EduationCard.tsx
+++ component/UI/EduationCard.tsx
@@ -2,19 +2,17 @@ import React from 'react';
 import { Fade } from 'react-awesome-reveal'
 import { IEducation } from '../../contexts/props';
 import { BiCalendar, BiBriefcase } from 'react-icons/bi';
-import '../../styles/Test.module.css';
+import styles from '../../styles/Education.module.css'
 
 const EductionCard2: React.FC<IEducation> = ({ id, course, startYear, endYear, detail }) => {
     return (
         <Fade direction='up'  triggerOnce={true}>
-            <div className="education-card">
-                
+            <div className={`${styles.education__card}`}> 
                 <div style={{ marginLeft: '1rem' }}>
                     <h3>{course}</h3>
                     <p>
                         {startYear} - {endYear === 'present' ? 'Current' : endYear}
                     </p>
-                    
                     <p>{detail}</p>
                 </div>
             </div>
